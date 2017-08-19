8c43111ebdbca5d7dd9d4de3e84329f225552847
yunilgeun <lk3436@naver.com>
2017-08-19 19:14:25 +0900

    Update models.py

--- quickdraw/models.py
+++ quickdraw/models.py
@@ -52,81 +52,87 @@ class LogisticModel(BaseModel):
         weights_regularizer=slim.l2_regularizer(l2_penalty))
     return {"predictions": output}
   
-  
 
 class ResnetModel(BaseModel):
 # %%
   def residual_network(self, model_input, n_outputs = 10,
-						 activation=tf.nn.relu, l2_penalty = 1e-8 ):
-	# %%
-	LayerBlock = namedtuple(
-		'LayerBlock', ['num_repeats', 'num_filters', 'bottleneck_size'])
-	blocks = [LayerBlock(3, 128, 32),
-			  LayerBlock(3, 256, 64),
-			  LayerBlock(3, 512, 128),
-			  LayerBlock(3, 1024, 256)]
-	# %%
-	input_shape = model_input.get_shape().as_list()
-	if len(input_shape) == 2:
-		ndim = int(sqrt(input_shape[1]))
-		if ndim * ndim != input_shape[1]:
-			raise ValueError('input_shape should be square')
-		model_input = tf.reshape(model_input, [-1, ndim, ndim, 1])
-	# %%
-	# First convolution expands to 64 channels and downsamples
-	net = conv2d(model_input, 64, k_h=7, k_w=7,
-				 name='conv1',
-				 activation=activation)
-	# %%
-	# Max pool and downsampling
-	net = tf.nn.max_pool(
-		net, [1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
-	# %%
-	# Setup first chain of resnets
-	net = conv2d(net, blocks[0].num_filters, k_h=1, k_w=1,
-				 stride_h=1, stride_w=1, padding='VALID', name='conv2')
-	# %%
-	# Loop through all res blocks
-	for block_i, block in enumerate(blocks):
-		for repeat_i in range(block.num_repeats):
+                     activation=tf.nn.relu, l2_penalty = 1e-8 ):
 
-			name = 'block_%d/repeat_%d' % (block_i, repeat_i)
-			conv = conv2d(net, block.bottleneck_size, k_h=1, k_w=1,
-						  padding='VALID', stride_h=1, stride_w=1,
-						  activation=activation,
-						  name=name + '/conv_in')
+    # %%
+    LayerBlock = namedtuple(
+        'LayerBlock', ['num_repeats', 'num_filters', 'bottleneck_size'])
+    blocks = [LayerBlock(3, 128, 32),
+              LayerBlock(3, 256, 64),
+              LayerBlock(3, 512, 128),
+              LayerBlock(3, 1024, 256)]
 
-			conv = conv2d(conv, block.bottleneck_size, k_h=3, k_w=3,
-						  padding='SAME', stride_h=1, stride_w=1,
-						  activation=activation,
-						  name=name + '/conv_bottleneck')
+    # %%
+    input_shape = model_input.get_shape().as_list()
+    if len(input_shape) == 2:
+        ndim = int(sqrt(input_shape[1]))
+        if ndim * ndim != input_shape[1]:
+            raise ValueError('input_shape should be square')
+        model_input = tf.reshape(model_input, [-1, ndim, ndim, 1])
 
-			conv = conv2d(conv, block.num_filters, k_h=1, k_w=1,
-						  padding='VALID', stride_h=1, stride_w=1,
-						  activation=activation,
-						  name=name + '/conv_out')
+    # %%
+    # First convolution expands to 64 channels and downsamples
+    net = conv2d(model_input, 64, k_h=7, k_w=7,
+                 name='conv1',
+                 activation=activation)
 
-			net = conv + net
-		try:
-			# upscale to the next block size
-			next_block = blocks[block_i + 1]
-			net = conv2d(net, next_block.num_filters, k_h=1, k_w=1,
-						 padding='SAME', stride_h=1, stride_w=1, bias=False,
-						 name='block_%d/conv_upscale' % block_i)
-		except IndexError:
-			pass
-	# %%
-	net = tf.nn.avg_pool(net,
-						 ksize=[1, net.get_shape().as_list()[1],
-								net.get_shape().as_list()[2], 1],
-						 strides=[1, 1, 1, 1], padding='VALID')
-	net = tf.reshape(
-		net,
-		[-1, net.get_shape().as_list()[1] *
-		 net.get_shape().as_list()[2] *
-		 net.get_shape().as_list()[3]])
-	
-	output = slim.fully_connected(
-		net, n_outputs, activation_fn = tf.nn.softmax,
-		weights_regularizer=slim.l2_regularizer(l2_penalty))
-	return {"predictions": output}
+    # %%
+    # Max pool and downsampling
+    net = tf.nn.max_pool(
+        net, [1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    # %%
+    # Setup first chain of resnets
+    net = conv2d(net, blocks[0].num_filters, k_h=1, k_w=1,
+                 stride_h=1, stride_w=1, padding='VALID', name='conv2')
+
+    # %%
+    # Loop through all res blocks
+    for block_i, block in enumerate(blocks):
+        for repeat_i in range(block.num_repeats):
+
+            name = 'block_%d/repeat_%d' % (block_i, repeat_i)
+            conv = conv2d(net, block.bottleneck_size, k_h=1, k_w=1,
+                          padding='VALID', stride_h=1, stride_w=1,
+                          activation=activation,
+                          name=name + '/conv_in')
+
+            conv = conv2d(conv, block.bottleneck_size, k_h=3, k_w=3,
+                          padding='SAME', stride_h=1, stride_w=1,
+                          activation=activation,
+                          name=name + '/conv_bottleneck')
+
+            conv = conv2d(conv, block.num_filters, k_h=1, k_w=1,
+                          padding='VALID', stride_h=1, stride_w=1,
+                          activation=activation,
+                          name=name + '/conv_out')
+
+            net = conv + net
+        try:
+            # upscale to the next block size
+            next_block = blocks[block_i + 1]
+            net = conv2d(net, next_block.num_filters, k_h=1, k_w=1,
+                         padding='SAME', stride_h=1, stride_w=1, bias=False,
+                         name='block_%d/conv_upscale' % block_i)
+        except IndexError:
+            pass
+
+    # %%
+    net = tf.nn.avg_pool(net,
+                         ksize=[1, net.get_shape().as_list()[1],
+                                net.get_shape().as_list()[2], 1],
+                         strides=[1, 1, 1, 1], padding='VALID')
+    net = tf.reshape(
+        net,
+        [-1, net.get_shape().as_list()[1] *
+         net.get_shape().as_list()[2] *
+         net.get_shape().as_list()[3]])
+
+    output = slim.fully_connected(
+        net, n_outputs, activation_fn = tf.nn.softmax,
+        weights_regularizer=slim.l2_regularizer(l2_penalty))
+    return {"predictions": output}
